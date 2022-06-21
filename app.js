b2c36c682f226b97633e4125d85d51118c0b613c
Toby2507 <98445016+Toby2507@users.noreply.github.com>
2022-06-21 15:30:50 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const firstNum = parseInt(prompt("Enter the first number: "));
-const secondNum = parseInt(prompt("Enter the second number: "));
+const firstNum = parseInt(prompt("Enter the first number: "), 10);
 const op = prompt("Enter an operator e.g + - * / : ");
+const secondNum = parseInt(prompt("Enter the second number: "), 10);
 
 if (op == "+") {
     alert(`Value is ${firstNum + secondNum}`)
